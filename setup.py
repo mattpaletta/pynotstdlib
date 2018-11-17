f1f4d47a374482367f3fd410e8121a2d33bfb88e
Matthew Paletta <mattpaletta@gmail.com>
2018-11-17 10:57:49 -0800

    Added setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynotstdlib",
+    version="0.0.1",
+    url='https://github.com/mattpaletta/pynotstdlib',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    setup_requires=[],
+    author="Matthew Paletta",
+    author_email="mattpaletta@gmail.com",
+    description="Extra useful functions and datatypes",
+    license="BSD",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Communications',
+    ]
+)
