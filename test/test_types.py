9e5fad91522ab458f88360e853cc7d5e522b94be
Matthew Paletta <mattpaletta@gmail.com>
2018-11-17 10:59:52 -0800

    Fixed tests

--- test/test_types.py
+++ test/test_types.py
@@ -5,10 +5,10 @@ from pynotstdlib.types import is_int, is_int_strict, is_bool
 
 class TestPQDict(TestCase):
     def test_int(self):
-        assert is_int("1"), "1 is an int"
+        assert is_int(1), "1 is an int"
 
     def test_int_strict(self):
-        assert not is_int("1.0", strict = True), "1.0 is not an int strict"
+        assert not is_int(1.0, strict = True), "1.0 is not an int strict"
         assert not is_int_strict(1.0), "1.0 is not an int strict"
 
     def test_bool(self):
