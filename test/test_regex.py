14a38ec85b3597e576e0fa801a93cd3e9813f276
Matthew Paletta <mattpaletta@gmail.com>
2019-03-07 11:30:01 -0800

    Added search function and test

--- test/test_regex.py
+++ test/test_regex.py
@@ -1,14 +1,13 @@
-from typing import re
 from unittest import TestCase
 
-from pynotstdlib.regex.regex import match
+from pynotstdlib.regex.regex import match, search
 
 
 class TestRegex(TestCase):
-    def test_empty(self):
-        result: re.Match = match(string = "", using = "")
+    def test_empty_match(self):
+        result = match(string = "", using = "")
         assert result.groups() == (), "Invalid empty string match"
 
-    def test_entire_string(self):
-        result: re.Match = match(string = "abc", using = "abc")
-        assert result.groups() == "abc", "Invalid match"
+    def test_search(self):
+        result = search(string = 'abcdef', using = '(?<=abc)def')
+        assert result.group(0) == "def", "Invalid match"
